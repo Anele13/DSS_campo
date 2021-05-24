15f2ae576ae6fa01c562b0fedd60e0950dd1d64c
anele <anelegaribaldi@gmail.com>
2021-05-24 12:43:49 -0300

    agregando el mapa

--- clima/views.py
+++ clima/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
-
+import json
 
 @login_required(login_url='login')
 def cargar_datos_climaticos(request):
     """
     Vista Inicio
     """
-    return render(request, "alta_datos_climaticos.html")
+    contexto={}
+    
+    #Deben ser mas sondas pero luego sacamos mas..
+    sondas = {'2':{'nombre':'28 de Julio','latitud':-43.24,'longitud': -65.48,'altura': 36},
+              '15':{'nombre':'Arroyo Pescado','latitud':-42.58,'longitud': -70.38,'altura': 545}}
+    contexto['sondas'] = json.dumps(sondas)
+
+    return render(request, "alta_datos_climaticos.html",contexto)
