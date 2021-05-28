9fba8ba6ac5e0d5bc8d2ad716f5afbfa9d569599
anele <anelegaribaldi@gmail.com>
2021-05-28 17:36:43 -0300

    cambios

--- mi_campo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import mostrar_filtros, buscador
-
-urlpatterns = [
-    path('mostrar_filtros', mostrar_filtros, name="mostrar_filtros"),
-    path('buscador', buscador, name="buscador"),
-]
