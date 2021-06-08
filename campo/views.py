759c7f51be103d1cbcd37029a85b623438ed4899
LuluAllende19 <abigail_lucii@hotmail.com>
2021-06-08 10:21:39 -0300

    Terminando forma de visualizar los resultados, y tratando de cambiarlaimagen de login

--- campo/views.py
+++ campo/views.py
@@ -33,64 +33,67 @@ def buscador(request):
 
 @login_required(login_url='login')
 def buscador(request):
-    contexto = {'anio': 2012,
-                'enero': {
-                    'promtemmax': 35,
-                    'promtemmin': 18,
-                    'dia1': {'tmpmax': 38, 'tmpmin': 21, 'precipitacion': 22},
-                    'dia2': {'tmpmax': 36, 'tmpmin': 17, 'precipitacion': 0},
-                    'dia3': {'tmpmax': 37, 'tmpmin': 18, 'precipitacion': 0},
-                    'dia4': {'tmpmax': 35, 'tmpmin': 20, 'precipitacion': 0},
-                    'dia5': {'tmpmax': 40, 'tmpmin': 22, 'precipitacion': 0},
-                    'dia6': {'tmpmax': 32, 'tmpmin': 16, 'precipitacion': 0},
-                    'dia7': {'tmpmax': 33, 'tmpmin': 16, 'precipitacion': 2},
-                    'dia8': {'tmpmax': 28, 'tmpmin': 14, 'precipitacion': 15},
-                    'dia9': {'tmpmax': 28, 'tmpmin': 14, 'precipitacion': 30},
-                    'dia10': {'tmpmax': 27, 'tmpmin': 15, 'precipitacion': 0}
-                },
-                'febrero': {
-                    'promtemmax': 31,
-                    'promtemmin': 15,
-                    'dia1': {'tmpmax': 31, 'tmpmin': 15, 'precipitacion': 5},
-                    'dia2': {'tmpmax': 32, 'tmpmin': 17, 'precipitacion': 0},
-                    'dia3': {'tmpmax': 33, 'tmpmin': 18, 'precipitacion': 0},
-                    'dia4': {'tmpmax': 35, 'tmpmin': 16, 'precipitacion': 0},
-                    'dia5': {'tmpmax': 28, 'tmpmin': 15, 'precipitacion': 10},
-                    'dia6': {'tmpmax': 29, 'tmpmin': 16, 'precipitacion': 0},
-                    'dia7': {'tmpmax': 25, 'tmpmin': 16, 'precipitacion': 2},
-                    'dia8': {'tmpmax': 26, 'tmpmin': 14, 'precipitacion': 15},
-                    'dia9': {'tmpmax': 28, 'tmpmin': 14, 'precipitacion': 30},
-                    'dia10': {'tmpmax': 27, 'tmpmin': 15, 'precipitacion': 0}
-                },
-                'marzo': {
-                    'promtemmax': 28,
-                    'promtemmin': 14,
-                    'dia1': {'tmpmax': 28, 'tmpmin': 15, 'precipitacion': 0},
-                    'dia2': {'tmpmax': 32, 'tmpmin': 16, 'precipitacion': 0},
-                    'dia3': {'tmpmax': 33, 'tmpmin': 18, 'precipitacion': 0},
-                    'dia4': {'tmpmax': 29, 'tmpmin': 15, 'precipitacion': 0},
-                    'dia5': {'tmpmax': 28, 'tmpmin': 15, 'precipitacion': 0},
-                    'dia6': {'tmpmax': 26, 'tmpmin': 14, 'precipitacion': 0},
-                    'dia7': {'tmpmax': 25, 'tmpmin': 16, 'precipitacion': 0},
-                    'dia8': {'tmpmax': 26, 'tmpmin': 13, 'precipitacion': 15},
-                    'dia9': {'tmpmax': 24, 'tmpmin': 14, 'precipitacion': 30},
-                    'dia10': {'tmpmax': 27, 'tmpmin': 15, 'precipitacion': 0}
-                },
-                'abril': {
-                    'promtemmax': 25,
-                    'promtemmin': 12,
-                    'dia1': {'tmpmax': 26, 'tmpmin': 13, 'precipitacion': 0},
-                    'dia2': {'tmpmax': 25, 'tmpmin': 14, 'precipitacion': 0},
-                    'dia3': {'tmpmax': 23, 'tmpmin': 11, 'precipitacion': 0},
-                    'dia4': {'tmpmax': 24, 'tmpmin': 12, 'precipitacion': 0},
-                    'dia5': {'tmpmax': 26, 'tmpmin': 15, 'precipitacion': 0},
-                    'dia6': {'tmpmax': 26, 'tmpmin': 14, 'precipitacion': 0},
-                    'dia7': {'tmpmax': 22, 'tmpmin': 10, 'precipitacion': 0},
-                    'dia8': {'tmpmax': 23, 'tmpmin': 13, 'precipitacion': 0},
-                    'dia9': {'tmpmax': 24, 'tmpmin': 14, 'precipitacion': 0},
-                    'dia10': {'tmpmax': 27, 'tmpmin': 15, 'precipitacion': 0}
-                },
-                }
+    contexto = {
+        'anio': 2012,
+        'meses': {
+            'enero': {
+                'promtemmax': 35,
+                'promtemmin': 18,
+                'dia1': {'tmpmax': 38, 'tmpmin': 21, 'precipitacion': 22},
+                'dia2': {'tmpmax': 36, 'tmpmin': 17, 'precipitacion': 0},
+                'dia3': {'tmpmax': 37, 'tmpmin': 18, 'precipitacion': 0},
+                'dia4': {'tmpmax': 35, 'tmpmin': 20, 'precipitacion': 0},
+                'dia5': {'tmpmax': 40, 'tmpmin': 22, 'precipitacion': 0},
+                'dia6': {'tmpmax': 32, 'tmpmin': 16, 'precipitacion': 0},
+                'dia7': {'tmpmax': 33, 'tmpmin': 16, 'precipitacion': 2},
+                'dia8': {'tmpmax': 28, 'tmpmin': 14, 'precipitacion': 15},
+                'dia9': {'tmpmax': 28, 'tmpmin': 14, 'precipitacion': 30},
+                'dia10': {'tmpmax': 27, 'tmpmin': 15, 'precipitacion': 0}
+            },
+            'febrero': {
+                'promtemmax': 31,
+                'promtemmin': 15,
+                'dia1': {'tmpmax': 31, 'tmpmin': 15, 'precipitacion': 5},
+                'dia2': {'tmpmax': 32, 'tmpmin': 17, 'precipitacion': 0},
+                'dia3': {'tmpmax': 33, 'tmpmin': 18, 'precipitacion': 0},
+                'dia4': {'tmpmax': 35, 'tmpmin': 16, 'precipitacion': 0},
+                'dia5': {'tmpmax': 28, 'tmpmin': 15, 'precipitacion': 10},
+                'dia6': {'tmpmax': 29, 'tmpmin': 16, 'precipitacion': 0},
+                'dia7': {'tmpmax': 25, 'tmpmin': 16, 'precipitacion': 2},
+                'dia8': {'tmpmax': 26, 'tmpmin': 14, 'precipitacion': 15},
+                'dia9': {'tmpmax': 28, 'tmpmin': 14, 'precipitacion': 30},
+                'dia10': {'tmpmax': 27, 'tmpmin': 15, 'precipitacion': 0}
+            },
+            'marzo': {
+                'promtemmax': 28,
+                'promtemmin': 14,
+                'dia1': {'tmpmax': 28, 'tmpmin': 15, 'precipitacion': 0},
+                'dia2': {'tmpmax': 32, 'tmpmin': 16, 'precipitacion': 0},
+                'dia3': {'tmpmax': 33, 'tmpmin': 18, 'precipitacion': 0},
+                'dia4': {'tmpmax': 29, 'tmpmin': 15, 'precipitacion': 0},
+                'dia5': {'tmpmax': 28, 'tmpmin': 15, 'precipitacion': 0},
+                'dia6': {'tmpmax': 26, 'tmpmin': 14, 'precipitacion': 0},
+                'dia7': {'tmpmax': 25, 'tmpmin': 16, 'precipitacion': 0},
+                'dia8': {'tmpmax': 26, 'tmpmin': 13, 'precipitacion': 15},
+                'dia9': {'tmpmax': 24, 'tmpmin': 14, 'precipitacion': 30},
+                'dia10': {'tmpmax': 27, 'tmpmin': 15, 'precipitacion': 0}
+            },
+            'abril': {
+                'promtemmax': 25,
+                'promtemmin': 12,
+                'dia1': {'tmpmax': 26, 'tmpmin': 13, 'precipitacion': 0},
+                'dia2': {'tmpmax': 25, 'tmpmin': 14, 'precipitacion': 0},
+                'dia3': {'tmpmax': 23, 'tmpmin': 11, 'precipitacion': 0},
+                'dia4': {'tmpmax': 24, 'tmpmin': 12, 'precipitacion': 0},
+                'dia5': {'tmpmax': 26, 'tmpmin': 15, 'precipitacion': 0},
+                'dia6': {'tmpmax': 26, 'tmpmin': 14, 'precipitacion': 0},
+                'dia7': {'tmpmax': 22, 'tmpmin': 10, 'precipitacion': 0},
+                'dia8': {'tmpmax': 23, 'tmpmin': 13, 'precipitacion': 0},
+                'dia9': {'tmpmax': 24, 'tmpmin': 14, 'precipitacion': 0},
+                'dia10': {'tmpmax': 27, 'tmpmin': 15, 'precipitacion': 0}
+            },
+        }
+    }
 
     contexto['resultados'] = json.dumps(contexto)
     return render(request, "resultados_filtros.html", contexto)
