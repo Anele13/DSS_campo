affdd5b5464b5e53bf23897d0fbaabd1e19ee1c0
anele <anelegaribaldi@gmail.com>
2021-07-14 22:59:19 -0300

    cambios

--- sistema_campo/settings.py
+++ sistema_campo/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = 'static'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
@@ -142,5 +142,3 @@ MEDIA_URL = '/img/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
