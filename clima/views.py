4edc2448918ec72c1e360493ade16fac336c9702
anele <anelegaribaldi@gmail.com>
2021-07-25 10:41:36 -0300

    agregando comprobacion en errores

--- clima/views.py
+++ clima/views.py
@@ -63,36 +63,37 @@ def format(dicc):
 
 @login_required(login_url='login')
 def estimacion_climatica(request):
+    try:
+        datos_climaticos = Sonda.objects.get(id=150).datos_climaticos_set.all().values('periodo', 'mm_lluvia')
+        df = pd.DataFrame(datos_climaticos)
+        df[['periodo']] = pd.to_datetime(df['periodo'])
+        df.set_index('periodo',inplace=True)
+        df.index.name = None
+        df = df['mm_lluvia'].resample('MS').mean()
+        df = df.fillna(df.bfill())
 
-    datos_climaticos = Sonda.objects.get(id=150).datos_climaticos_set.all().values('periodo', 'mm_lluvia')
-    df = pd.DataFrame(datos_climaticos)
-    df[['periodo']] = pd.to_datetime(df['periodo'])
-    df.set_index('periodo',inplace=True)
-    df.index.name = None
-    df = df['mm_lluvia'].resample('MS').mean()
-    df = df.fillna(df.bfill())
+        #cargo el modelo entrenado
+        path = staticfiles_storage.path('data/modelo.pickle')
+        results = load_pickle(path)
 
+        #datos originales
+        datos_sonda = format(df.to_dict())
 
-    #cargo el modelo entrenado
-    path = staticfiles_storage.path('data/modelo.pickle')
-    results = load_pickle(path)
+        #estimacion climatica
+        pred_uc = results.get_forecast(steps=10)
+        datos_prediccion = format(pred_uc.predicted_mean.to_dict())
 
-    #datos originales
-    datos_sonda = format(df.to_dict())
+        #intervalos de confianza
+        pred_ci = pred_uc.conf_int()
+        intervalo_bajo = format(pred_ci[['lower mm_lluvia']].to_dict()['lower mm_lluvia'])
+        intervalo_alto =format(pred_ci[['upper mm_lluvia']].to_dict()['upper mm_lluvia'])
 
-    #estimacion climatica
-    pred_uc = results.get_forecast(steps=10)
-    datos_prediccion = format(pred_uc.predicted_mean.to_dict())
-
-    #intervalos de confianza
-    pred_ci = pred_uc.conf_int()
-    intervalo_bajo = format(pred_ci[['lower mm_lluvia']].to_dict()['lower mm_lluvia'])
-    intervalo_alto =format(pred_ci[['upper mm_lluvia']].to_dict()['upper mm_lluvia'])
-
-    
-    contexto={}
-    contexto["datos_sonda"] = json.dumps(datos_sonda)
-    contexto["datos_prediccion"] = json.dumps(datos_prediccion)
-    contexto["intervalo_bajo"] = json.dumps(intervalo_bajo)
-    contexto["intervalo_alto"] = json.dumps(intervalo_alto)
+        
+        contexto={}
+        contexto["datos_sonda"] = json.dumps(datos_sonda)
+        contexto["datos_prediccion"] = json.dumps(datos_prediccion)
+        contexto["intervalo_bajo"] = json.dumps(intervalo_bajo)
+        contexto["intervalo_alto"] = json.dumps(intervalo_alto)
+    except Exception as e:
+        messages.warning(request,str(e))
     return render(request, "estimacion_climatica.html", contexto)
