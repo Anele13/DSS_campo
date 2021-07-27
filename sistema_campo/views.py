b5de969ef021d333ce6f97d17ed7268741b4c76a
LuluAllende19 <abigail_lucii@hotmail.com>
2021-07-27 16:27:05 -0300

    Inicio - Grafico de lluvias del año del ultimo registro

--- sistema_campo/views.py
+++ sistema_campo/views.py
@@ -1,24 +1,25 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models.functions import ExtractMonth
 from django.contrib.auth import authenticate, login
-from django.contrib import messages
+from django.shortcuts import render, redirect
 from usuario.views import get_persona_campo
+from django.contrib import messages
 from campo.models import Campo
+
+import calendar
+import json
 
 CANT_ANIMALES_HA = 15
 
 
-def devolver_hacienda(request):
+def devolver_hacienda(datos_produccion):
 
-    campo = Campo.objects.get(persona=request.user.persona)
-    datos_produccion = campo.datos_produccion_set.all()
     ultimo_registro = datos_produccion.order_by('-periodo')[0]
     return ultimo_registro.cantidad_ovejas, ultimo_registro.cantidad_carneros, ultimo_registro.cantidad_corderos, ultimo_registro.cantidad_muertes_corderos
 
 
-def calcular_ocupacion(request):
-    campo = Campo.objects.get(persona=request.user.persona)
-    datos_produccion = campo.datos_produccion_set.all()
+def calcular_ocupacion(campo, datos_produccion):
+
     ultimo_registro = datos_produccion.order_by('-periodo')[0]
 
     animales_ideales = campo.cant_hectareas * CANT_ANIMALES_HA
@@ -32,13 +33,34 @@ def calcular_ocupacion(request):
     return cantidad_ha_ocupadas, cantidad_ha_libres
 
 
+def devolver_lluvias_mensuales(campo, datos_produccion, datos_climaticos):
+
+    nombres_meses = []
+    lluvias_mensuales = []
+
+    anio_actual = datos_produccion.values(
+        'periodo__year').order_by('-periodo')[0]['periodo__year']
+    datos_climaticos_anio_actual = datos_climaticos.filter(
+        periodo__year=anio_actual).order_by('periodo')
+    datos_climaticos_meses = sorted(datos_climaticos_anio_actual.annotate(month=ExtractMonth(
+        'periodo')).values_list('month', flat=True).distinct())
+
+    d_1 = datos_climaticos_anio_actual.values('periodo__month', 'mm_lluvia',)
+
+    for mes in list(set(datos_climaticos_meses)):
+        d2 = list(filter(lambda d: d['periodo__month'] == mes, d_1))
+        nombres_meses.append(calendar.month_name[mes])
+        lluvia_acumulada = round(sum([d['mm_lluvia'] for d in d2]), 2)
+        lluvias_mensuales.append(lluvia_acumulada)
+
+    return nombres_meses, lluvias_mensuales
+
+
 @login_required(login_url='login')
 def inicio(request):
     """
     Vista Inicio
     """
-    resultado = {}
-    produccion = {}
     contexto = {}
     user = request.user
     persona, campo = get_persona_campo(user)
@@ -55,11 +77,20 @@ def inicio(request):
             request, "Debe cargar los datos de produccion de su campo.")
 
     else:
+
+        campo = Campo.objects.get(persona=request.user.persona)
+        datos_climaticos = campo.sonda.datos_climaticos_set.all()
+        datos_produccion = campo.datos_produccion_set.all()
+
         cantidad_ovejas, cantidad_carneros, cantidad_corderos, mortandad = devolver_hacienda(
-            request)
-        ha_ocupadas, ha_libres = calcular_ocupacion(request)
+            datos_produccion)
+        ha_ocupadas, ha_libres = calcular_ocupacion(campo, datos_produccion)
+        nombres_meses, lluvias_mensuales = devolver_lluvias_mensuales(
+            campo, datos_produccion, datos_climaticos)
+
         contexto = {'cantidad_ovejas': cantidad_ovejas, 'cantidad_carneros': cantidad_carneros,
-                    'cantidad_corderos': cantidad_corderos, 'mortandad': mortandad, 'ha_ocupadas': ha_ocupadas, 'ha_libres': ha_libres}
+                    'cantidad_corderos': cantidad_corderos, 'mortandad': mortandad, 'ha_ocupadas': ha_ocupadas,
+                    'ha_libres': ha_libres, 'nombres_meses': json.dumps(nombres_meses), 'lluvias_mensuales': json.dumps(lluvias_mensuales)}
     return render(request, "bienvenido.html", contexto)
 
 
