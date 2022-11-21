7a219e64a73400af27fe7ec9360b999792300b26
Lucila Allende <abigail_lucii@hotmail.com>
2022-11-21 09:48:54 -0300

    add routes, and templates prediction

--- prediccion/urls.py
+++ prediccion/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from prediccion.views import prediccion_lana
+from prediccion.views import predecir
 
 urlpatterns = [
-    path('prediccion_lana', prediccion_lana, name="prediccion_lana"),
+    path('predecir', predecir, name="predecir"),
+    path('predecir/<str:query>', predecir, name="predecir"),
 ]
--- prediccion/views.py
+++ prediccion/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
+def predecir_por_filtro(filtro):
+    label_estimacion = None
+    label_datos = None
+    flag_filtro = None
+    if filtro == 'lana':
+        print('Lanaa')
+        flag_filtro = 'Kg de lana'
+    elif filtro== 'corderos':
+        print('Corderos')
+        flag_filtro = 'Cantidad de corderos'
+    return label_estimacion, label_datos, flag_filtro
+
 @login_required(login_url='login')
-def prediccion_lana(request):
-    contexto = {
-        'msg':'Hola'
-    }
+def predecir(request,query='lana'):
+    label_datos, label_estimacion, flag_filtro = predecir_por_filtro(query)
+    contexto = {}
+    contexto["label_datos"] = label_datos
+    contexto["label_estimacion"] = label_estimacion
+    contexto["flag_filtro"] = flag_filtro
     return render(request, "prediccion.html", contexto)
