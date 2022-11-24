e24ebfc62c11c42efbc60470257e8a096b5ff0d1
anele <anele_13@hotmail.com>
2022-11-24 18:00:59 -0300

    fixed buscar mejor año por condicion

--- campo/views.py
+++ campo/views.py
@@ -32,52 +32,114 @@ def get_nombre_mes(numero_mes):
 
 def get_mejor_año_por_condicion(query, datos_produccion, datos_climaticos):
     # para el maximo valor es order by con el - adelante.
+    mejor_año = 2010
+    
     if query == 'rinde':
-        return datos_produccion.values('periodo__year').\
-            annotate(rinde_anual=Max('rinde_lana')).\
-            order_by('-rinde_anual')[0]['periodo__year']
+        datos_produccion.rinde_lana = datos_produccion.rinde_lana.astype(int)
+        mejor_año = datos_produccion[['year','rinde_lana']].\
+            groupby("year").\
+            max().\
+            reset_index().\
+            sort_values(by=['rinde_lana'], ascending=False, na_position='last').\
+            head(1).\
+            year.\
+            values[0]
 
     if query == 'finura':
-        return datos_produccion.filter(finura_lana__gt=0).values('periodo__year').\
-            annotate(finura_anual=Min('finura_lana')).\
-            order_by('finura_anual')[0]['periodo__year']  # TODO checkaear
+        datos_produccion.finura_lana = datos_produccion.finura_lana.astype(int)
+        mejor_año = datos_produccion[['year','finura_lana']].\
+            groupby("year").\
+            max().\
+            reset_index().\
+            sort_values(by=['finura_lana'], ascending=False, na_position='last').\
+            head(1).\
+            year.\
+            values[0]
 
     if query == 'lluvia':
-        return datos_climaticos.values('periodo__year').\
-            annotate(mm_lluvia_anual=Sum('mm_lluvia')).\
-            order_by('-mm_lluvia_anual')[0]['periodo__year']
+        datos_climaticos.mm_lluvia = datos_climaticos.mm_lluvia.astype(int)
+        mejor_año = datos_climaticos[['year','mm_lluvia']].\
+            groupby("year").\
+            max().\
+            reset_index().\
+            sort_values(by=['mm_lluvia'], ascending=False, na_position='last').\
+            head(1).\
+            year.\
+            values[0]
 
     if query == 'temperatura':
-        max_temperatura = datos_climaticos.aggregate(Max('temperatura_maxima'))[
-            'temperatura_maxima__max']
-        mejor_año_temperatura = datos_climaticos.filter(
-            temperatura_maxima=max_temperatura)[0].periodo.year
-        return mejor_año_temperatura
+        datos_climaticos.temperatura_maxima = datos_climaticos.temperatura_maxima.astype(int)
+        mejor_año = datos_climaticos[['year','temperatura_maxima']].\
+            groupby("year").\
+            max().\
+            reset_index().\
+            sort_values(by=['temperatura_maxima'], ascending=False, na_position='last').\
+            head(1).\
+            year.\
+            values[0]
 
     if query == 'mortandad':
-        return datos_produccion.values('periodo__year').\
-            annotate(mortandad_anual=Sum('cantidad_muertes_corderos')).\
-            order_by('mortandad_anual')[0]['periodo__year']
+        datos_produccion.muertes = datos_produccion.muertes.astype(int)
+        mejor_año = datos_produccion[['year','muertes']].\
+            groupby("year").\
+            sum().\
+            reset_index().\
+            sort_values(by=['muertes'], ascending=False, na_position='last').\
+            head(1).\
+            year.\
+            values[0]
 
     if query == 'lana':
-        return datos_produccion.values('periodo__year').\
-            annotate(kg_lana_anual=Sum('cantidad_lana_producida')).\
-            order_by('-kg_lana_anual')[0]['periodo__year']
+        datos_produccion.lana_producida = datos_produccion.lana_producida.astype(int)
+        mejor_año = datos_produccion[['year','lana_producida']].\
+            groupby("year").\
+            sum().\
+            reset_index().\
+            sort_values(by=['lana_producida'], ascending=False, na_position='last').\
+            head(1).\
+            year.\
+            values[0]
 
+    """
     if query == 'carne':
         return datos_produccion.values('periodo__year').\
             annotate(kg_carne_anual=Sum('cantidad_carne_producida')).\
             order_by('-kg_carne_anual')[0]['periodo__year']
+        
+        datos_produccion.pariciones = datos_produccion.pariciones.astype(int)
+        mejor_año = datos_produccion[['year','pariciones']].\
+            groupby("year").\
+            sum().\
+            reset_index().\
+            sort_values(by=['pariciones'], ascending=False, na_position='last').\
+            head(1).\
+            year.\
+            values[0]
+    """ #NO esta mas carne como variable 
 
     if query == 'pariciones':
-        return datos_produccion.values('periodo__year').\
-            annotate(pariciones_anual=Sum('cantidad_pariciones')).\
-            order_by('-pariciones_anual')[0]['periodo__year']
+        datos_produccion.pariciones = datos_produccion.pariciones.astype(int)
+        mejor_año = datos_produccion[['year','pariciones']].\
+            groupby("year").\
+            sum().\
+            reset_index().\
+            sort_values(by=['pariciones'], ascending=False, na_position='last').\
+            head(1).\
+            year.\
+            values[0]
 
     if query == 'hacienda':
-        return datos_produccion.values('periodo__yeobjectar').\
-            annotate(cantidad_ovejas_anual=Max('cantidad_ovejas')).\
-            order_by('-cantidad_ovejas_anual')[0]['periodo__year']
+        datos_produccion.ovejas = datos_produccion.ovejas.astype(int)
+        mejor_año = datos_produccion[['year','ovejas']].\
+            groupby("year").\
+            max().\
+            reset_index().\
+            sort_values(by=['ovejas'], ascending=False, na_position='last').\
+            head(1).\
+            year.\
+            values[0]
+
+    return mejor_año
 
 
 def validar(valor):
@@ -113,20 +175,19 @@ def mi_campo(request, query='rinde'):
         datos_produccion = df[['periodo','corderos','ovejas','carneros','pariciones','muertes','lana_producida','carne_producida','rinde_lana','finura_lana']]
         datos_climaticos = df[['periodo','temperatura_minima','temperatura_maxima','humedad','velocidad_viento','direccion_viento','mm_lluvia','localidad']]
 
-        #mejor_año = get_mejor_año_por_condicion(query, datos_produccion, datos_climaticos)
-        mejor_año = 2010 #TODO esto no va. solucionar funcion de arriba
-        
-        #datos = datos_climaticos.filter(periodo__year=mejor_año).order_by('periodo')
-        #datos_prod = datos_produccion.filter(periodo__year=mejor_año).order_by('periodo')
+
+        datos_climaticos['day'] = datos_climaticos.periodo.apply(lambda row:  row.day)
+        datos_climaticos['month'] = datos_climaticos.periodo.apply(lambda row:  row.month)
+        datos_climaticos['year'] = datos_climaticos.periodo.apply(lambda row:  row.year)
+
+        datos_produccion['month'] = datos_produccion.periodo.apply(lambda row:  row.month)
+        datos_produccion['day'] = datos_produccion.periodo.apply(lambda row:  row.day)
+        datos_produccion['year'] = datos_produccion.periodo.apply(lambda row:  row.year)
+
+        mejor_año = get_mejor_año_por_condicion(query, datos_produccion, datos_climaticos)
+
         datos = datos_climaticos[datos_climaticos.periodo.dt.year == mejor_año]
-        datos['day'] = datos.periodo.apply(lambda row:  row.day)
-        datos['month'] = datos.periodo.apply(lambda row:  row.month)
-
-
-
         datos_prod = datos_produccion[datos_produccion.periodo.dt.year == mejor_año]
-        datos_prod['month'] = datos_prod.periodo.apply(lambda row:  row.month)
-        datos_prod['day'] = datos_prod.periodo.apply(lambda row:  row.day)
 
 
         meses = sorted(list(set([d for d in datos.periodo.dt.month])))
